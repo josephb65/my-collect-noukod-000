9a717175281eb265012cc6259298f5f4dc042baf
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-15 12:52:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(arr) 
-   i = 0
+  count = 0
   result = []
 
-  while i < array.size
-    block_given? # optional
-    result << yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
-    i += 1
+  while count < arr.size
+    block_given? 
+    res << yield(arr[count]) 
+    count += 1
   end
 result
 end 
